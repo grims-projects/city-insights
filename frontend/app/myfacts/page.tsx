cd80b5b8b514b84741388ddf4c84c802d49463b3
Grim <grim.e.kalman@gmail.com>
2023-12-08 10:33:34 +0100

    Refactor

--- frontend/app/myfacts/page.tsx
+++ frontend/app/myfacts/page.tsx
@@ -7,39 +7,31 @@ import InsightCard from "../components/insightCard/InsightCard";
 import axios from "axios";
 
 type CityInsight = {
-    id: string;
-    cityName: string;
-    cityDescription: string;
-    insightText: string;
-    imageUrl: string;
-    wikiUrl: string;
-  };
+  id: string;
+  cityName: string;
+  cityDescription: string;
+  insightText: string;
+  imageUrl: string;
+  wikiUrl: string;
+};
 
-  const MyFacts: React.FC = () => {
-    const [cityInsights, setCityInsights] = useState<CityInsight[]>([]);
+export default function MyFacts() {
+  const [cityInsights, setCityInsights] = useState<CityInsight[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await axios.get<CityInsight[]>("http://localhost:8080/api/insight/city");
-        setCityInsights(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+      await axios.get<CityInsight[]>("http://localhost:8080/api/insight/city")
+        .then(response => setCityInsights(response.data))
+        .catch(error => console.error("Error fetching data:", error));
     };
-
     fetchData();
   }, []);
 
   const handleDelete = async (id: string) => {
-    try {
-      await axios.delete(`http://localhost:8080/api/insight/city/${id}`);
-      setCityInsights(cityInsights.filter(insight => insight.id !== id));
-    } catch (error) {
-      console.error("Error deleting insight:", error);
-    }
+    await axios.delete(`http://localhost:8080/api/insight/city/${id}`)
+      .then(() => setCityInsights(cityInsights.filter(insight => insight.id !== id)))
+      .catch(error => console.error("Error deleting insight:", error));
   };
-
 
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between ${garamond.className} pl-16 pr-16 pt-6 pb-[84px]`}>
@@ -47,9 +39,9 @@ type CityInsight = {
         <InsightCard key={insight.id} id={insight.id} cityName={insight.cityName} imageUrl={insight.imageUrl} insightText={insight.insightText} onDelete={() => handleDelete(insight.id)} />
       ))}
       <div className="fixed bottom-0 w-full z-10">
-        <Navbar setIsButtonClicked={Function}/>
+        <Navbar setIsButtonClicked={Function} />
       </div>
     </main>
-  );
+  )
 }
-export default MyFacts;
+
